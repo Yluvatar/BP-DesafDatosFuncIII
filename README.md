d925182fb2dad48ceb88cfcc74b74046f17ee4cd
Juan Pablo Scheihing <eru.yluvatar@gmail.com>
2024-03-16 01:14:48 -0300

    Commit Desafío corregido y funcional v1.1

---
+++ README.md
@@ -0,0 +1,9 @@
+# BP-DesafDatosFuncIII
+
+Bootcamp Python 2024
+
+Desafío Datos y Funciones III
+
+Juan Pablo Scheihing
+
+https://github.com/Yluvatar/BP-DesafDatosFuncIII
---
+++ validador.py
@@ -0,0 +1,16 @@
+def validate(opciones, eleccion):
+    # Definir validación de eleccion
+    ##########################################################################
+    while eleccion not in opciones:
+        print('Opción no válida, ingrese una de las opciones válidas: ')
+        eleccion = input('Ingresa una opcion: ')
+    ##########################################################################
+    return eleccion
+
+
+if __name__ == '__main__':
+    eleccion = input('Ingresa una Opción: ').lower()
+    # letras = ['a','b','c','d'] # pueden probar con letras también para verificar su funcionamiento.
+    numeros = ['0', '1', '2', '3', 'y', 'n']
+    # Si se ingresan valores no validos a eleccion debe seguir preguntando
+    validate(numeros, eleccion)
